b76937b1b6884d13a0c4c880803b852448edb53e
i-nirbhay10 <nirbhayverma10@gmail.com>
2023-06-13 23:00:00 +0530

    created userschema, user registratin and login functionality

--- server/app.js
+++ server/app.js
@@ -2,23 +2,16 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-
 dotenv.config({ path: "./.env" });
+require("./db/connection");
+// const User = require("./userregister/datafom");
+
+//for json data reeding
+app.use(express.json());
+// router require
+app.use(require("./router/routs"));
 
 const port = process.env.PORT;
-
-// db connection
-
-const URI = process.env.DATABASE;
-
-mongoose
-  .connect(URI)
-  .then(() => {
-    console.log("database connection successfull");
-  })
-  .catch((error) => console.log(error));
-
-// use middeleware
 
 middeleware = (req, res, next) => {
   console.log("this is middelwere response");
@@ -27,32 +20,30 @@ middeleware = (req, res, next) => {
 
 // get routing
 
-app.get("/", (req, res) => {
-  res.send("hello world this is home page created by nirbhay");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello world this is home page created by nirbhay");
+// });
 
-app.get("/about", middeleware, (req, res) => {
-  res.send("hello world this about page is created by nirbhay");
-  console.log("this is about response");
-});
+// app.get("/about", middeleware, (req, res) => {
+//   res.send("hello world this about page is created by nirbhay");
+//   console.log("this is about response");
+// });
 
-app.get("/service", (req, res) => {
-  res.send("hello world this is service page created by nirbhay");
-});
+// app.get("/service", (req, res) => {
+//   res.send("hello world this is service page created by nirbhay");
+// });
 
-app.get("/login", (req, res) => {
-  res.send("hello world this is login page created by nirbhay");
-});
+// app.get("/login", (req, res) => {
+//   res.send("hello world this is login page created by nirbhay");
+// });
 
-app.get("/singup", (req, res) => {
-  res.send("hello world this is signup page created by nirbhay");
-});
+// app.get("/singup", (req, res) => {
+//   res.send("hello world this is signup page created by nirbhay");
+// });
 
-app.get("/profile", (req, res) => {
-  res.send("hello world this is profile page after login created by nirbhay");
-});
-
-console.log("working");
+// app.get("/profile", (req, res) => {
+//   res.send("hello world this is profile page after login created by nirbhay");
+// });
 
 app.listen(port, () => {
   console.log(`app renning on port no ${port}`);
---
+++ server/userregister/datafom.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    // unique: true,
+  },
+  work: {
+    type: String,
+    required: true,
+  },
+  mobailno: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  console.log("hi i am bcrypt");
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cpassword = await bcrypt.hash(this.cpassword, 12);
+  }
+  next();
+});
+
+const User = mongoose.model("Registration", userSchema);
+
+module.exports = User;
